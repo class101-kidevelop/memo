e681d171821f2302658062d31ee83f2a0a0d7565
class101-kidevelop <stanford@kakao.com>
2021-12-11 02:12:54 +0900

    add: MemoList & Item components

--- src/app/components/Memo/List/index.tsx
+++ src/app/components/Memo/List/index.tsx
@@ -1,12 +1,42 @@
 import styled from 'styled-components';
+import MemoItem from '../Item';
 
 const List = styled.div`
   width: 300px;
   height: calc(100vh - 60px);
   border-right: 1px solid #e9e9e9;
   padding: 0 10px;
+
+  @media (max-width: 687px) {
+    margin-left: -200px;
+    transition: 0.2s;
+    &:hover {
+      margin-left: 0;
+    }
+  }
 `;
 
 export default function MemoList() {
-  return <List></List>;
+  return (
+    <List>
+      <MemoItem
+        id="1"
+        created_at={new Date().toString()}
+        selected={true}
+        preview="나는 메모입니다."
+      />
+      <MemoItem
+        id="1"
+        created_at={new Date().toString()}
+        selected={false}
+        preview="나는 메모입니다."
+      />
+      <MemoItem
+        id="1"
+        created_at={new Date().toString()}
+        selected={false}
+        preview="나는 메모입니다."
+      />
+    </List>
+  );
 }
